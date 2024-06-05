045ad1cddef75324aa1547224bd4ba9c13e7eda8
tsolmon khorloo <tsolmonkhorloo@tsolmons-MacBook-Pro.local>
2024-06-05 15:26:39 +0800

    fix: online store code

---
+++ src/Exception/OnlineStoreNotFoundException.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Nextstore\SyliusDropshippingCorePlugin\Exception;
+
+class OnlineStoreNotFoundException extends \RuntimeException implements ExceptionInterface
+{
+}
