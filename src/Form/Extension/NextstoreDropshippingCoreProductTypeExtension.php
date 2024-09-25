77d39fba3345705d8421d6842e64c994457721fa
muggystyle24 <muggystyle24@gmail.com>
2024-09-25 15:55:04 +0800

    Product Form fix

--- src/Form/Extension/NextstoreDropshippingCoreProductTypeExtension.php
+++ src/Form/Extension/NextstoreDropshippingCoreProductTypeExtension.php
@@ -6,7 +6,6 @@ namespace Nextstore\SyliusDropshippingCorePlugin\Form\Extension;
 
 use Nextstore\SyliusDropshippingCorePlugin\Model\ProductInterface;
 use Sylius\Bundle\ProductBundle\Form\Type\ProductType;
-use Sylius\Bundle\MoneyBundle\Form\Type\MoneyType;
 use Symfony\Component\Form\AbstractTypeExtension;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
@@ -30,15 +29,13 @@ final class NextstoreDropshippingCoreProductTypeExtension extends AbstractTypeEx
                     'nextstore_sylius_dropshipping_core.ui.order_type_order' => ProductInterface::ORDER_TYPE_ORDER,
                 ]
             ])
-            ->add('priceOt', MoneyType::class, [
+            ->add('priceOt', TextType::class, [
                 'label' => 'nextstore_sylius_dropshipping_core.ui.price_ot',
                 'required' => false,
-                'currency' => $options['channel']->getBaseCurrency()->getCode(),
             ])
-            ->add('originalPriceOt', MoneyType::class, [
+            ->add('originalPriceOt', TextType::class, [
                 'label' => 'nextstore_sylius_dropshipping_core.ui.original_price_ot',
                 'required' => false,
-                'currency' => $options['channel']->getBaseCurrency()->getCode(),
             ])
         ;
 
